8c27bd542f79c633d9666e069cd0d859b0aa16cb
EDUARDO VILAS BOAS DE BARROS <23111780@dsn127m1246730.cfp127aluno.local>
2024-09-05 17:24:38 -0300

    Rick problemático

---
+++ app/src/main/java/com/example/rickandmorty/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.rickandmorty
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import com.example.rickandmorty.screens.CharacterDetails
+import com.example.rickandmorty.screens.ListAllCharacters
+import com.example.rickandmorty.ui.theme.RickAndMortyTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //enableEdgeToEdge()
+        setContent {
+            RickAndMortyTheme {
+                ListAllCharacters()
+                //CharacterDetails()
+            }
+        }
+    }
+}
